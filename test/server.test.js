4456f151642aa3c6c7f2fc7b4c0133fdfc1033f6
Cynthia Li <cynthiax@yahoo-inc.com>
2014-07-15 14:55:20 -0700

    make it fail

--- test/server.test.js
+++ test/server.test.js
@@ -11,6 +11,6 @@ Y.TestRunner.add(new Y.TestCase({
             res = { json: function (obj) { result = obj; } };
         server(null, res, null);
         Assert.isObject(result);
-        Assert.areEqual('world', result.hello);
+        Assert.areEqual('worldy', result.hello);
     }
 }));
